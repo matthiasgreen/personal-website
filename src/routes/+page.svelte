c28ed8c06f0debf8f50c5dfca104dbc2bcb03944
Matthias Green <matthiasgreen2@gmail.com>
2026-05-30 01:29:42 +0200

    Add Djungelskog

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome to SvelteKit.</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
+<p>Website under construction, bear with me...</p>
+
+<p>
+	<a
+		href="https://commons.wikimedia.org/wiki/File:Djungelskog_bear_wearing_a_cowboy_hat_and_flannel.jpg#/media/File:Djungelskog_bear_wearing_a_cowboy_hat_and_flannel.jpg"
+		><img
+			src="https://upload.wikimedia.org/wikipedia/commons/6/62/Djungelskog_bear_wearing_a_cowboy_hat_and_flannel.jpg"
+			alt="Djungelskog bear wearing a cowboy hat and flannel.jpg"
+			style='max-width: 600px'
+		/></a
+	><br />By
+	<a
+		href="//commons.wikimedia.org/w/index.php?title=User:Jsw618&amp;action=edit&amp;redlink=1"
+		class="new"
+		title="User:Jsw618 (page does not exist)">Jsw618</a
+	>
+	- <span class="int-own-work" lang="en">Own work</span>,
+	<a href="https://creativecommons.org/licenses/by/4.0" title="Creative Commons Attribution 4.0"
+		>CC BY 4.0</a
+	>, <a href="https://commons.wikimedia.org/w/index.php?curid=187679090">Link</a>
+</p>
